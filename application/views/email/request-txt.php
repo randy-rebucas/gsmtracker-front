500e082c4afa1c61b5596939b2973e80eb1fced3
Randy rebucas <rebucasrandy1986@gmail.com>
2019-09-02 08:16:30 +0800

    remove ald files

--- application/views/email/request-txt.php
+++
@@ -1,5 +0,0 @@
-From: <?php echo $request_name; ?> (<?php echo $request_name; ?>)
-Message: <?php echo $request_message; ?>
-
---
-This e-mail was send from a reqeust form on <?php echo $site_name; ?> (http://clinicplus.com)
--- js/print.js
+++
@@ -1,6 +0,0 @@
-var BASE_URL = document.getElementsByTagName("body")[0].getAttribute("id");
-
-function closePrint(record_id) {
-    window.opener.location.replace(BASE_URL + 'queings/process/' + record_id);
-    window.close();
-}
