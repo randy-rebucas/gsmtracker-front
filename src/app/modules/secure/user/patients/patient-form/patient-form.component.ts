39f46e1d56800bd00397fbdf89a0cd712b3ffcf8
randy rebucas <rebucasrandy1986@gmail.com>
2020-03-08 15:53:02 +0800

    update language

--- src/app/modules/secure/user/patients/patient-form/patient-form.component.ts
+++ src/app/modules/secure/user/patients/patient-form/patient-form.component.ts
@@ -69,7 +69,7 @@ export class PatientFormComponent implements OnInit {
         validators: [
           Validators.required,
           Validators.pattern('^[0-9]*$'),
-          // Validators.minLength(9),
+          Validators.minLength(9),
           Validators.maxLength(11)
         ]
       }),
